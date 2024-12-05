0855a8256cc2ff7f54500f2ee6385093f548c220
UrregoJontahan <https://github.com/UrregoJontahan>
2024-12-05 17:52:18 -0500

    primer commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,10 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'prueba',
-  webDir: 'dist'
+  appId: 'com.example.patners',
+  appName: 'patners',
+  webDir: 'dist',
+  bundledWebRuntime: false,
 };
 
 export default config;
